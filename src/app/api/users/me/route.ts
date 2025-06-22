5bae77f30f48f0da8c5b9080f800e4d4ae9909bc
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-22 20:07:54 +0530

    initial commit

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,18 @@
+import {connect} from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import {NextRequest,NextResponse} from 'next/server'
+import { getDataFromToken } from '@/helpers/getDataFromToken'
+
+connect();
+
+export async function POST(request: NextRequest){
+    const userId = await getDataFromToken(request)
+    const user = User.findById({_id: userId}).select("-password")
+
+    return NextResponse.json({
+        message: "User fetched successfully",
+        data: user
+    })
+
+
+}
